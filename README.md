7abc72a0321a503a727ea5cb4b7c5232235fe7d1
Jonatha <johnninajoao@gmail.com>
2023-06-22 19:11:10 -0300

    Adição de midlewares e finalização

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # login
 Login and Register 
+
+# bcrypt
+criptografia da senha salva no banco de dados
+
+# passport 
+autenticação dos dados do usuário
+
+Um formato simples porém eficaz para registro e login de usuários. 
+
+
+
--- server.js
+++ server.js
@@ -1,8 +1,30 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
 const express = require("express")
 const app = express()
 const bcrypt = require("bcrypt")
+const passport = require("passport")
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+
+//Conexão com banco de dados
 const connection = require("./database/database")
 const Register = require("./database/Register")
+
+const initializePassport = require("./passportConfig")
+
+initializePassport(passport, async (email) => {
+  try {
+    const user = await Register.findOne({ where: { email: email } });
+    return user;
+  } catch (error) {
+    throw error;
+  }
+});
+
 
 connection
     .authenticate()
@@ -15,26 +37,37 @@ connection
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false}))
 app.use(express.json())
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
 
 app.use(express.static('public'))
 
-app.get("/", (req,res)=>{
-    res.render('index', {name: 'John'})
+app.get("/",checkAuthenticated, (req,res)=>{
+    res.render('index', {name: req.user.name})
 })
 
-app.get("/login", (req,res)=>{
+app.get("/login",checkNotAuthenticated, (req,res)=>{
     res.render('login')
 })
 
-app.post("/login", (req,res)=>{
-    
-})
+app.post("/login", checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
 
-app.get("/register", (req,res)=>{
+app.get("/register", checkNotAuthenticated, (req,res)=>{
     res.render('register')
 })
 
-app.post("/register", async (req,res)=>{
+app.post("/register", checkNotAuthenticated, async (req,res)=>{
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
         await Register.create({
@@ -42,11 +75,29 @@ app.post("/register", async (req,res)=>{
             email: req.body.email,
             password: hashedPassword
         })   
-        res.redirect('/login')
+        res.redirect('/')
     } catch(err) {
         console.log("erro ao criar registro"+err);
         res.redirect('/register')
     }
 })
 
+app.delete("/logout", (req,res)=>{
+    req.logOut(()=>{
+        res.redirect('/login')
+    })
+})
+
+function checkAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return next()
+    }
+    res.redirect('/login')
+}
+function checkNotAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return res.redirect('/')
+    }
+    next()
+}
 app.listen(3000)
